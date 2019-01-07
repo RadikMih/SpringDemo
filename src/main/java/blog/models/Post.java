dbeb23c700fe7bbd56f7b99d53eaabbe194293bd
RadikMih <radik.mihnev@gmail.com>
2019-01-07 17:09:29 +0200

    adding methods to PostController

--- src/main/java/blog/models/Post.java
+++ src/main/java/blog/models/Post.java
@@ -1,4 +1,22 @@
 package blog.models;
 
 public class Post {
+    private int id;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/blog/web/PostController.java
+++ src/main/java/blog/web/PostController.java
@@ -1,4 +1,88 @@
 package blog.web;
 
+
+import blog.models.Post;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+curl -d ‘{“author”:“Ivan”, “content”:“message is cool”}’ -H “Content-Type: application/json”
+-X POST http://localhost:8080/api/blogposts/
+curl -d -H "Content-Type: application/json" -X DELETE localhost:8080/api/restaurants/2
+
+curl -d '{"name": "Pri Krasi"}' -H "Content-Type: application/json" -X POST localhost:8080/api/restaurants/
+curl -d '{"name": "Pri Krasi"}' -H "Content-Type: application/json" -X PUT localhost:8080/api/restaurants/3
+ */
+
+
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+    List<Post> posts;
+
+    public PostController(){
+        posts = new ArrayList<>();
+    }
+
+    @RequestMapping("/")
+    public List<Post> listPosts(){
+        return posts;
+    }
+
+     @RequestMapping(
+            value = "/{id}",
+             method = RequestMethod.GET
+     )
+    public Post findById(@PathVariable("id") String idString){
+        int id = Integer.parseInt(idString);
+        return posts.stream()
+                .filter(post -> post.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.POST
+    )
+    public void addPost(@RequestBody Post post){
+        int maxId = posts.stream()
+                .mapToInt(Post::getId)
+                .max()
+                .orElse(0);
+        int newId = maxId + 1;
+        post.setId(newId);
+        posts.add(post);
+    }
+
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.PUT
+    )
+    public void update(@PathVariable("id") String idString, @RequestBody Post updatePost){
+        Post post = findById(idString);
+        post.setText(updatePost.getText());
+    }
+
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void remove(@PathVariable("id") String idString){
+        int id = Integer.parseInt(idString);
+        int index = -1;
+        for (int i = 0; i < posts.size(); i++) {
+            if (posts.get(i).getId() == id){
+                index = i;
+                break;
+            }
+        }
+        posts.remove(index);
+    }
+
+
+
 }
